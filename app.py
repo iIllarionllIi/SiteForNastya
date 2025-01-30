bbfe91aa10e3efa39d14159f91292f3cbde71fa5
iIllarionllIi <lillarionllil@gmail.com>
2025-01-31 01:40:26 +0300

    Push

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def admin_update_services():
     return redirect(url_for('admin_dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080)
